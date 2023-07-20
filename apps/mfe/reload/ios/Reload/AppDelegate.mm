3bfb82bf643600929fe39f572fa0be90256b142b
nazri ghazi <nazri.ghazi@celcomdigi.com>
2023-07-20 12:24:06 +0800

    upgrade reload react native vers to 0.72.3

--- apps/mfe/reload/ios/Reload/AppDelegate.mm
+++ apps/mfe/reload/ios/Reload/AppDelegate.mm
@@ -17,7 +17,7 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"src/main"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
